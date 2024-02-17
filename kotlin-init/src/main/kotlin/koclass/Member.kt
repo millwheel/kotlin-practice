1f17dec794bf35125a7bd5528ebe5a0a18b39e8b
season <sionwer4@naver.com>
2024-02-17 22:35:04 +0900

    feat: custom getter 추가

--- kotlin-init/src/main/kotlin/koclass/Member.kt
+++ kotlin-init/src/main/kotlin/koclass/Member.kt
@@ -6,6 +6,10 @@ class Member(
     val name: String = "김마리",
     var age: Int = 1 // 부생성자 사용보다는 default parameter를 쓰는 것이 낫다.
 ) {
+
+    val upperCaseName: String // custom getter를 사용하여 밖에서 볼 때 별도의 property가 존재하는 것처럼 할 수 있다.
+        get() = this.name.uppercase()
+
     init{
         if (age <= 0){
             throw IllegalArgumentException("나이는 ${age}일 수 없습니다.")
