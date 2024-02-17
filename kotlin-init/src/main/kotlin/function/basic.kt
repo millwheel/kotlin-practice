cfb36bf89f55c57ed7a1bb54f6c5ba962d45149b
season <sionwer4@naver.com>
2024-02-17 20:21:59 +0900

    feat: add default parameter function

--- kotlin-init/src/main/kotlin/function/basic.kt
+++ kotlin-init/src/main/kotlin/function/basic.kt
@@ -3,3 +3,19 @@ package function
 // 코틀린에서는 중괄호와 return이 아래처럼 대체 가능함
 // 중괄화 대신 = 를 쓰면 타입도 생략 가능
 fun max(a:Int, b:Int) = if (a > b){ a } else { b }
+
+
+// 코틀린에는 함수 자체에도 기본값을 사용할 수 있다.
+fun repeat(
+    str: String,
+    num: Int = 3,
+    useNewLine: Boolean = true
+){
+    for(i in 1..num){
+        if(useNewLine) {
+            println(str)
+        } else {
+            print(str)
+        }
+    }
+}
