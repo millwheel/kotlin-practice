bf8804930b2582ff74636c14540ff68e8bdc207e
season <sionwer4@naver.com>
2024-02-17 18:58:54 +0900

    feat: case 문 추가

--- kotlin-init/src/main/kotlin/Main.kt
+++ kotlin-init/src/main/kotlin/Main.kt
@@ -18,6 +18,17 @@ fun validateScore(score: Int){
     }
 }
 
+// case 문. 어떠한 expression도 들어갈 수 있다.
+// 코틀린에서 하나의 문장은 statement가 아니라 expression이다.
+fun getGradeWithSwitch(score: Int):String{
+    return when (score){
+        in 90..99 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
+        else -> "D"
+    }
+}
+
 
 // null이 들어갈 수 없는 함수
 fun startWithA1(str:String): Boolean{
