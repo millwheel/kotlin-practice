f49562288e38497ea3307b65694b84af04829742
season <sionwer4@naver.com>
2024-02-17 20:04:18 +0900

    refactor: parse 에서 throw 제거

--- kotlin-init/src/main/kotlin/function/parse.kt
+++ kotlin-init/src/main/kotlin/function/parse.kt
@@ -3,10 +3,10 @@ package function
 import java.lang.IllegalArgumentException
 import java.lang.NumberFormatException
 
-fun parseIntOrThrow(str: String): Int{
-    try{
-        return str.toInt() // 형변환
+fun parseIntOrThrow(str: String): Int?{
+    return try{
+        str.toInt() // 형변환
     } catch (e: NumberFormatException){
-        throw IllegalArgumentException("주어진 ${str}은 숫자가 아닙니다.")
+        null
     }
 }
