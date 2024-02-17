1fda4fa304b87de5b8a79b9de830b9dbc3e0e8c5
season <sionwer4@naver.com>
2024-02-17 22:45:22 +0900

    feat: animal kotlin class

---
+++ kotlin-init/src/main/kotlin/koclass/Cat.kt
@@ -0,0 +1,11 @@
+package koclass
+
+class Cat(
+    species: String
+) : Animal(species, 4) { // 상속 받을 때는 한 칸 띄우고 콜론을 쓴다.
+
+    // override 필수적으로 붙여야 한다.
+    override fun move() {
+        print("고양이 움직인다")
+    }
+}
