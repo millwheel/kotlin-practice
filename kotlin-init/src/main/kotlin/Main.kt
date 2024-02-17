81792f6eb5686caa182be828303c49414d03f3fc
season <sionwer4@naver.com>
2024-02-17 17:29:01 +0900

    comment: 주석 수정

--- kotlin-init/src/main/kotlin/Main.kt
+++ kotlin-init/src/main/kotlin/Main.kt
@@ -10,12 +10,12 @@ fun startWithA1(str:String): Boolean{
     return str.startsWith("A");
 }
 
-// null이 들어갈 수 있는 함수. Elvis 연산자
+// null이 들어갈 수 있는 함수. safe call과 Elvis 연산자
 fun startWithA2(str: String?):Boolean{
     return str?.startsWith("A") ?: throw IllegalArgumentException("null이 들어왔습니다.")
 }
 
-// null이 들어갈 수 있는 함수. safe call
+// null이 들어갈 수 있는 함수. safe call과 리턴타입
 fun startWithA3(str: String?):Boolean?{
     return str?.startsWith("A")
 }
