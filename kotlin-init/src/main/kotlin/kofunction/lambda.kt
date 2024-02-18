9fb1106faaa61c280d8ad8f5deafb606b456de7e
season <sionwer4@naver.com>
2024-02-18 16:20:56 +0900

    feat: Add code using lambda as parameter of function

--- kotlin-init/src/main/kotlin/kofunction/lambda.kt
+++ kotlin-init/src/main/kotlin/kofunction/lambda.kt
@@ -14,9 +14,23 @@ fun lambda(){
 
     isApple(fruits[0])
     isApple.invoke(fruits[0])
+
+    filterFruits(fruits, isApple) // 람다 함수를 값처럼 사용하기
 }
 
 class Fruit(
     val name: String,
     val price: Int
 )
+
+private fun filterFruits(
+    fruits: List<Fruit>, filter: (Fruit) -> Boolean
+): List<Fruit> {
+    val results = mutableListOf<Fruit>()
+    for (fruit in fruits){
+        if (filter(fruit)){
+            results.add(fruit)
+        }
+    }
+    return results
+}
