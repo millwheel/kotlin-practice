0b18c735be597c2cfc1948906f9f6b5e16807ba0
season <sionwer4@naver.com>
2024-02-18 16:15:56 +0900

    feat: Create lambda kt

---
+++ kotlin-init/src/main/kotlin/kofunction/lambda.kt
@@ -0,0 +1,19 @@
+package kofunction
+
+fun lambda(){
+    val fruits = listOf(
+        Fruit("사과", 1000),
+        Fruit("수박", 20000)
+    )
+
+    val isApple = fun(fruit: Fruit): Boolean{
+        return fruit.name == "사과"
+    }
+
+    val isApple2 = {fruit: Fruit -> fruit.name == "사과"}
+}
+
+class Fruit(
+    val name: String,
+    val price: Int
+)
