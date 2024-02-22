9ee35b63a53cc5efe45d980d563c1ae951c3c71a
season <sionwer4@naver.com>
2024-02-22 23:30:00 +0900

    feat: rate limit service simple의 기능 구현

--- bank-app/bank/src/main/java/com/example/bank/service/ratelimit/RateLimitServiceSimple.java
+++ bank-app/bank/src/main/java/com/example/bank/service/ratelimit/RateLimitServiceSimple.java
@@ -2,14 +2,24 @@ package com.example.bank.service.ratelimit;
 
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class RateLimitServiceSimple implements RateLimitService {
 
-    private ConcurrentHashMap<String, RequestCounter> requestCounter = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, RequestCounter> requestCounts = new ConcurrentHashMap<>();
+    private final int MAX_REQUESTS = 100;
+    private final Duration WINDOW_DURATION = Duration.ofMinutes(1);
 
     public boolean isAllowed(String key){
+        RequestCounter requestCounter = requestCounts.computeIfAbsent(key, k -> new RequestCounter(WINDOW_DURATION));
+
+        requestCounter.incrementRequestCount();
+        if (requestCounter.isLimitExceeded(MAX_REQUESTS)) {
+                return false;
+        }
+
         return true;
     }
 }
