beee9a3a022371059bd7192340b5d60e3c27b8c5
박시준 <sjpark@logblack.com>
2024-02-23 20:13:41 +0900

    feat: other bank project 생성

---
+++ bank-app/otherbank/src/main/kotlin/com/example/otherbank/OtherbankApplication.kt
@@ -0,0 +1,11 @@
+package com.example.otherbank
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OtherbankApplication
+
+fun main(args: Array<String>) {
+	runApplication<OtherbankApplication>(*args)
+}
---
+++ bank-app/otherbank/src/test/kotlin/com/example/otherbank/OtherbankApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.otherbank
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OtherbankApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
