121c4031e97dde1ac4130b35326071e1b06189e0
season <sionwer4@naver.com>
2024-02-23 13:56:39 +0900

    refactor: Rate limit constant duration 수정

--- bank-app/bank/src/main/java/com/example/bank/service/ratelimit/RateLimitConstant.java
+++ bank-app/bank/src/main/java/com/example/bank/service/ratelimit/RateLimitConstant.java
@@ -5,6 +5,6 @@ import java.time.Duration;
 public class RateLimitConstant {
 
     public static final int MAX_REQUESTS = 10;
-    public static final Duration WINDOW_DURATION = Duration.ofMinutes(1);
+    public static final Duration WINDOW_DURATION = Duration.ofSeconds(10);
 
 }
