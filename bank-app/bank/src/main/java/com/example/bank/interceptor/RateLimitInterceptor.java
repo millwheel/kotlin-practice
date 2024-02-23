5935ebf3a92759aed8c4d6e6578f2fd980893209
season <sionwer4@naver.com>
2024-02-23 13:53:47 +0900

    feat: rate limiter interceptor response body 추가

--- bank-app/bank/src/main/java/com/example/bank/interceptor/RateLimitInterceptor.java
+++ bank-app/bank/src/main/java/com/example/bank/interceptor/RateLimitInterceptor.java
@@ -5,7 +5,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.io.IOException;
 
 public class RateLimitInterceptor implements HandlerInterceptor {
 
@@ -21,6 +24,15 @@ public class RateLimitInterceptor implements HandlerInterceptor {
         String clientIp = request.getRemoteAddr();
         if (!rateLimitService.isAllowed(clientIp)){
             response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            String responseBody = "Too many request. Please try again later.";
+            try {
+                response.getWriter().write(responseBody);
+                response.getWriter().flush();
+                response.getWriter().close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
             return false;
         }
         return true;
